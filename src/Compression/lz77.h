3823a1f431b594b7873e51b7117eceff1cfa48d5
Alexander <84857900+4z0t@users.noreply.github.com>
2022-11-17 16:57:16 +0300

    put sliding window in lz77

--- src/Compression/lz77.h
+++ src/Compression/lz77.h
@@ -23,78 +23,147 @@ namespace Compression
 	}
 
 
-	class  SlidingWindow
+
+
+	namespace LZ77
 	{
-	public:
 
-
-		SlidingWindow();
-		SlidingWindow(unit size);
-		~SlidingWindow();
-
-		void ProcessRef(BytesVector& output)
+		class  SlidingWindow
 		{
-			for (unit i = 0; i < _ref.len; i++)
+		public:
+
+
+			SlidingWindow();
+			SlidingWindow(unit size);
+			~SlidingWindow();
+
+			void UnpackRef(BytesVector& output)
 			{
-				size_t end = _window.size() - 1;
-				unit u = _window[end - _ref.offset];
-				PushWindow(u);
-				output.push_back(u);
-			}
-		}
-
-		bool Decode(unit u, BytesVector& out)
-		{
-			if (u == REF_UNIT) {
-				if (_isRef) //ref symbol twice
+				for (unit i = 0; i < _ref.len; i++)
 				{
-					out.push_back(REF_UNIT);
-					PushWindow(REF_UNIT);
-					_isRef = false;
-					return true;
+					size_t end = _window.size() - 1;
+					unit u = _window[end - _ref.offset];
+					PushWindow(u);
+					output.push_back(u);
 				}
-				_isRef = true;
-				return false;
 			}
-			if (_isRef)
+
+			bool Decode(unit u, BytesVector& out)
 			{
-				if (_ref.len == 0)
-				{
-					_ref.len = u;
+				if (u == REF_UNIT) {
+					if (_isRef) //ref symbol twice
+					{
+						out.push_back(REF_UNIT);
+						PushWindow(REF_UNIT);
+						_isRef = false;
+						return true;
+					}
+					_isRef = true;
 					return false;
 				}
-				_ref.offset = u;
-				this->ProcessRef(out);
-				_isRef = false;
-				_ref.len = 0;
+				if (_isRef)
+				{
+					if (_ref.len == 0)
+					{
+						_ref.len = u;
+						return false;
+					}
+					_ref.offset = u;
+					this->UnpackRef(out);
+					_isRef = false;
+					_ref.len = 0;
+					return true;
+				}
+				PushWindow(u);
+				out.push_back(u);
 				return true;
 			}
-			PushWindow(u);
-			out.push_back(u);
-			return true;
-		}
 
-		bool Encode(unit u, BytesVector& out)
-		{
-
-			Ref prev = _ref;
-			_msg.push_back(u);
-
-			if (Match())
+			bool Encode(unit u, BytesVector& out)
 			{
-				if (_msg.size() == _size)
+
+				Ref prev = _ref;
+				_msg.push_back(u);
+
+				if (Match())
 				{
-					out = End();
-					_msg.clear();
+					if (_msg.size() == _size)
+					{
+						out = End();
+						_msg.clear();
+					}
 				}
-			}
-			else
-			{
-				_msg.pop_back();
-				if (_msg.size() > 2)
+				else
 				{
-					//put ref:
-					PushRef(out, prev);
+					_msg.pop_back();
+					if (_msg.size() > 2)
+					{
+						//put ref:
+						PushRef(out, prev);
+					}
+					else
+					{
+						for (unit uo : _msg)
+						{
+							if (uo == REF_UNIT)
+								out.push_back(REF_UNIT);
+							out.push_back(uo);
+						}
+
+					}
+					_msg.clear();
+					_msg.push_back(u);
+				}
+				PushWindow(u);
+
+				return out.size() != 0;
+			}
+
+
+			bool Match()
+			{
+				_ref.len = 0;
+				_ref.offset = 0;
+
+				unit length = 0;
+				unit pos = 0;
+				for (unit i = 0; i < _window.size(); i++)
+				{
+					if (_msg.size() == length)//whole msg fits
+					{
+						_ref.len = length;
+						_ref.offset = _window.size() - i;
+						break;
+					}
+
+					if (_msg[length] == _window[i])//continue if match
+					{
+						length++;
+					}
+					else
+					{
+						//if (_matchLength < length + 1)//find the largest
+						//{
+						//	_matchLength = length + 1;
+						//	_matchPos = i - length;
+						//}
+						//reset
+						i -= length;
+						length = 0;
+					}
+				}
+				//return if we found anything
+				return _ref.len != 0;
+			}
+
+
+
+			BytesVector End()
+			{
+				BytesVector out;
+				if (_ref.len != 0)
+				{
+					PushRef(out, _ref);
 				}
 				else
 				{
@@ -104,123 +173,55 @@ namespace Compression
 							out.push_back(REF_UNIT);
 						out.push_back(uo);
 					}
-
 				}
-				_msg.clear();
-				_msg.push_back(u);
+				return out;
 			}
-			PushWindow(u);
-
-			return out.size() != 0;
-		}
 
 
-		bool Match()
-		{
-			_ref.len = 0;
-			_ref.offset = 0;
-
-			unit length = 0;
-			unit pos = 0;
-			for (unit i = 0; i < _window.size(); i++)
+			void PushWindow(unit u)
 			{
-				if (_msg.size() == length)//whole msg fits
+				if (_window.size() == _size)
 				{
-					_ref.len = length;
-					_ref.offset = _window.size() - i;
-					break;
+					ShiftLeft(_window, 1);
 				}
-
-				if (_msg[length] == _window[i])//continue if match
-				{
-					length++;
-				}
-				else
-				{
-					//if (_matchLength < length + 1)//find the largest
-					//{
-					//	_matchLength = length + 1;
-					//	_matchPos = i - length;
-					//}
-					//reset
-					i -= length;
-					length = 0;
-				}
+				_window.push_back(u);
 			}
-			//return if we found anything
-			return _ref.len != 0;
-		}
 
 
 
-		BytesVector End()
-		{
-			BytesVector out;
-			if (_ref.len != 0)
+
+		private:
+
+			BytesVector _msg;							//message being processed
+			BytesVector _window;						//sliding window itself
+			const unit _size = SLIDING_WINDOW_SIZE;		//size
+			bool _isRef = false;
+			struct Ref {
+				unit len = 0;
+				unit offset = 0;
+			} _ref;
+
+			static void PushRef(BytesVector& out, Ref ref)
 			{
-				PushRef(out, _ref);
+				out.push_back(REF_UNIT);
+				out.push_back(ref.len);
+				out.push_back(ref.offset);
 			}
-			else
-			{
-				for (unit uo : _msg)
-				{
-					if (uo == REF_UNIT)
-						out.push_back(REF_UNIT);
-					out.push_back(uo);
-				}
-			}
-			return out;
-		}
+		};
 
-
-		void PushWindow(unit u)
+		SlidingWindow::SlidingWindow(unit size) : _size(size)
 		{
-			if (_window.size() == _size)
-			{
-				ShiftLeft(_window, 1);
-			}
-			_window.push_back(u);
+			_window.reserve(_size);
 		}
 
-
-
-
-	private:
-
-		BytesVector _msg;							//message being processed
-		BytesVector _window;						//sliding window itself
-		const unit _size = SLIDING_WINDOW_SIZE;		//size
-		bool _isRef = false;
-		struct Ref {
-			unit len = 0;
-			unit offset = 0;
-		} _ref;
-
-		static void PushRef(BytesVector& out, Ref ref)
+		SlidingWindow::SlidingWindow() : SlidingWindow(SLIDING_WINDOW_SIZE)
 		{
-			out.push_back(REF_UNIT);
-			out.push_back(ref.len);
-			out.push_back(ref.offset);
 		}
-	};
 
-	SlidingWindow::SlidingWindow(unit size) : _size(size)
-	{
-		_window.reserve(_size);
-	}
+		SlidingWindow::~SlidingWindow()
+		{
 
-	SlidingWindow::SlidingWindow() : SlidingWindow(SLIDING_WINDOW_SIZE)
-	{
-	}
-
-	SlidingWindow::~SlidingWindow()
-	{
-
-	}
-
-
-	namespace LZ77
-	{
+		}
 
 		BytesVector Compress(const BytesVector& input)
 		{
@@ -248,10 +249,10 @@ namespace Compression
 		{
 			BytesVector res;
 			BytesVector cache;
-			SlidingWindow w;
+			SlidingWindow window;
 			for (size_t i = 0; i < input.size(); i++)
 			{
-				if (w.Decode(input[i], cache))
+				if (window.Decode(input[i], cache))
 				{
 					res.insert(res.end(), cache.begin(), cache.end());
 					cache.clear();
