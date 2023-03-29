dabf67540e23b4b4e36193c9c17c62506dbd4aa6
4z0t <gastec139@gmail.com>
2023-03-29 18:05:12 +0300

    encode in bitmap

--- src/test_BMP/bmp.cpp
+++ src/test_BMP/bmp.cpp
@@ -21,77 +21,42 @@ BitsAndBytes::BytesVector RandomBytes(size_t size)
 }
 
 
-void encoder(Path input_path, Path key_path, Path output_path)
+uint32_t EncodeByteInQuad(uint32_t quad, uint8_t byte)
 {
-	using namespace BitsAndBytes;
-	size_t e;
-	size_t n;
+	union
 	{
-		File key_file(key_path);
-		if (!key_file.Open(true))
-		{
-			cout << "Couldn't open file " << key_path << endl;
-			return;
-		}
+		struct {
+			uint8_t red;
+			uint8_t green;
+			uint8_t blue;
+			uint8_t alpha;
+		};
+		uint32_t quad = quad;
+	}pixel;
 
-		if (key_file.GetSize() != sizeof(size_t) * 2)
-		{
-			cout << "Key file has wrong size" << endl;
-			return;
-		}
-
-		e = key_file.Read<size_t>();
-		n = key_file.Read<size_t>();
-	}
-
-
-
-	File input_file(input_path);
-	if (!input_file.Open(true))
-	{
-		cout << "Couldn't open file " << input_path << endl;
-		return;
-	}
-
-	File output_file(output_path);
-	if (!output_file.Open(false))
-	{
-		cout << "Couldn't open file " << output_path << endl;
-		return;
-	}
-
-
-
-	for (size_t i = 0; i < input_file.GetSize(); i++)
-	{
-		
-	}
+	pixel.blue &= 0xFC;
+	pixel.blue |= (byte >> 6) & 0x3;
+	pixel.green &= 0xFC;
+	pixel.green |= (byte >> 4) & 0x3;
+	pixel.red &= 0xFC;
+	pixel.red |= (byte >> 2) & 0x3;
+	pixel.alpha &= 0xFC;
+	pixel.alpha |= (byte) & 0x3;
+	return pixel.quad;
 }
 
-void decoder(Path input_path, Path key_path, Path output_path)
+
+void encoder(Path input_path, Path bmp_path, Path output_path)
 {
 	using namespace BitsAndBytes;
-	size_t d;
-	size_t n;
+
+
+	File bmp_file(bmp_path);
+	if (!bmp_file.Open(true))
 	{
-		File key_file(key_path);
-		if (!key_file.Open(true))
-		{
-			cout << "Couldn't open file " << key_path << endl;
-			return;
-		}
-
-		if (key_file.GetSize() != sizeof(size_t) * 2)
-		{
-			cout << "Key file has wrong size" << endl;
-			return;
-		}
-
-		d = key_file.Read<size_t>();
-		n = key_file.Read<size_t>();
+		cout << "Couldn't open file " << bmp_path << endl;
+		return;
 	}
-
-
 
 	File input_file(input_path);
 	if (!input_file.Open(true))
@@ -109,11 +74,57 @@ void decoder(Path input_path, Path key_path, Path output_path)
 
 
 
-	for (size_t i = 0; i < input_file.GetSize() / sizeof(size_t); i++)
+	uint32_t offset = 0;
+
+	for (size_t i = 0; i < 10; )
 	{
-		
-		output_file.Put(v);
+		output_file.Put(bmp_file.Get());
 	}
+
+	offset = bmp_file.Read<uint32_t>();
+	output_file.Write(offset);
+	std::cout << "Offset: " << offset << std::endl;
+
+	for (size_t i = 14; i < offset; i++)
+	{
+		output_file.Put(bmp_file.Get());
+	}
+
+	uint32_t bmp_file_size = bmp_file.GetSize();
+	for (size_t i = offset; i < bmp_file_size; i += 4)
+	{
+		output_file.Write(EncodeByteInQuad(bmp_file.Read<uint32_t>(), input_file.Get()));
+	}
+	std::cout << "successfully encoded file!" << std::endl;
+
+}
+
+void decoder(Path input_path, Path bmp_path)
+{
+	using namespace BitsAndBytes;
+
+	File key_file(bmp_path);
+	if (!key_file.Open(true))
+	{
+		cout << "Couldn't open file " << bmp_path << endl;
+		return;
+	}
+
+
+
+
+
+
+	File input_file(input_path);
+	if (!input_file.Open(true))
+	{
+		cout << "Couldn't open file " << input_path << endl;
+		return;
+	}
+
+
+
+
 }
 
 
@@ -134,12 +145,11 @@ int main(int argc, char** argv)
 	if (flag == "-decode")
 	{
 
-		decoder(input_path, bmp_path, output_path);
+		decoder(input_path, bmp_path);
 	}
 	else if (flag == "-encode")
 	{
 		encoder(input_path, bmp_path, output_path);
-
 	}
 	else
 	{
